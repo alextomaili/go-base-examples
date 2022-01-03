93b02b4e465de2833edf3cbfeb08489ea9b3ee6f
alextomaili <a.tomaili@propellerads.net>
2022-01-03 23:08:16 +0300

    ...

--- pkg/contention/contention_test.go
+++ pkg/contention/contention_test.go
@@ -5,16 +5,26 @@ import (
 	"testing"
 )
 
+var readers = [...]int{1, 5, 10, 20, 30, 40, 50}
+
 func BenchmarkRwStorage(b *testing.B) {
-	for _, r := range []int{1, 5, 10, 20, 30, 40, 50, 100} {
+	for _, r := range readers {
 		b.Run("RwStorage-w4-r"+strconv.Itoa(r), func(b *testing.B) {
 			AggregateTest(b, NewRwStorage(), 4, r)
 		})
 	}
 }
 
+func BenchmarkRwSlStorage(b *testing.B) {
+	for _, r := range readers {
+		b.Run("RwSlStorage(1024)-w4-r"+strconv.Itoa(r), func(b *testing.B) {
+			AggregateTest(b, NewRwSlStorage(10), 4, r)
+		})
+	}
+}
+
 func BenchmarkSyncMapStorage(b *testing.B) {
-	for _, r := range []int{1, 5, 10, 20, 30, 40, 50, 100} {
+	for _, r := range readers {
 		b.Run("SyncMapStorage-w4-r"+strconv.Itoa(r), func(b *testing.B) {
 			AggregateTest(b, NewSyncMapStorage(), 4, r)
 		})
---
+++ pkg/contention/rw_sl_storage.go
@@ -0,0 +1,49 @@
+package contention
+
+import "sync"
+
+type (
+	RwSlStorage struct {
+		bn         int
+		mutex      []sync.RWMutex
+		aggregates []map[Key]int64
+	}
+)
+
+func NewRwSlStorage(bnPov2 int) *RwSlStorage {
+	bn := 2 << (bnPov2 - 1)
+	r := &RwSlStorage{
+		bn:         bn,
+		mutex:      make([]sync.RWMutex, 0, bn),
+		aggregates: make([]map[Key]int64, 0, bn),
+	}
+	for i := 0; i < bn; i++ {
+		r.mutex = append(r.mutex, sync.RWMutex{})
+		r.aggregates = append(r.aggregates, make(map[Key]int64))
+	}
+	return r
+}
+
+func (s *RwSlStorage) hashSlot(k Key) int {
+	h := k.x<<16 + k.y
+	h = h ^ (h >> 16) // from java.util.HashMap, java 1.8
+	return h & (s.bn - 1)
+}
+
+func (s *RwSlStorage) Apply(msg Message) {
+	k := msg.Key
+	i := s.hashSlot(k)
+
+	s.mutex[i].Lock()
+	s.aggregates[i][k] += msg.Value
+	s.mutex[i].Unlock()
+}
+
+func (s *RwSlStorage) Get(k Key) int64 {
+	i := s.hashSlot(k)
+
+	s.mutex[i].RLock()
+	r := s.aggregates[i][k]
+	s.mutex[i].RUnlock()
+	return r
+}
