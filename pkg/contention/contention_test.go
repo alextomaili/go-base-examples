f0bee7b4c39b81b2f14a74a7e1151ba8417e4457
alextomaili <a.tomaili@propellerads.net>
2022-01-05 02:23:22 +0300

    fix

--- pkg/contention/contention_test.go
+++ pkg/contention/contention_test.go
@@ -42,21 +42,6 @@ func BenchmarkSyncMapStorage(b *testing.B) {
 	}
 }
 
-func BenchmarkBIncrStorage(b *testing.B) {
-	b.Run("BIncrStorage-w1-r0", func(b *testing.B) {
-		storage := NewBIncrStorage(1, NewRwSlStorage(10))
-		AggregateTest(b, storage, 1, 0, waitReaders)
-		//b.Logf("batch generation %v", storage.BatchGeneration())
-	})
-	for _, r := range readers {
-		b.Run("BIncrStorage-w4-r"+strconv.Itoa(r), func(b *testing.B) {
-			storage := NewBIncrStorage(writers, NewRwSlStorage(10))
-			AggregateTest(b, storage, writers, r, waitReaders)
-			//b.Logf("batch generation %v", storage.BatchGeneration())
-		})
-	}
-}
-
 func BenchmarkBatchStorage(b *testing.B) {
 	b.Run("BatchStorage-w1-r0", func(b *testing.B) {
 		storage := NewBatchStorage(1, NewRwSlStorage(10))
@@ -66,6 +51,21 @@ func BenchmarkBatchStorage(b *testing.B) {
 	for _, r := range readers {
 		b.Run("BatchStorage-w4-r"+strconv.Itoa(r), func(b *testing.B) {
 			storage := NewBatchStorage(writers, NewRwSlStorage(10))
+			AggregateTest(b, storage, writers, r, waitReaders)
+			//b.Logf("batch generation %v", storage.BatchGeneration())
+		})
+	}
+}
+
+func BenchmarkBIncrStorage(b *testing.B) {
+	b.Run("BIncrStorage-w1-r0", func(b *testing.B) {
+		storage := NewBIncrStorage(1, NewRwSlStorage(10))
+		AggregateTest(b, storage, 1, 0, waitReaders)
+		//b.Logf("batch generation %v", storage.BatchGeneration())
+	})
+	for _, r := range readers {
+		b.Run("BIncrStorage-w4-r"+strconv.Itoa(r), func(b *testing.B) {
+			storage := NewBIncrStorage(writers, NewRwSlStorage(10))
 			AggregateTest(b, storage, writers, r, waitReaders)
 			//b.Logf("batch generation %v", storage.BatchGeneration())
 		})
