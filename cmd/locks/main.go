e1557f63f90c77ac7a5203261ee9d34c9e52ff5e
alextomaili <a.tomaili@propellerads.net>
2024-12-05 22:18:19 +0200

    ....

--- cmd/locks/main.go
+++ cmd/locks/main.go
@@ -3,9 +3,32 @@ package main
 import (
 	"fmt"
 	"github.com/alextomaili/go-base-examples/pkg/accounts_store"
+	"sync"
+	"time"
 )
 
+func mTest() {
+	m := sync.Mutex{}
+
+	i := 100
+	f := func() {
+		m.Lock()
+		i = i + 1
+		time.Sleep(time.Millisecond * 300)
+		m.Unlock()
+	}
+
+	go f()
+	f()
+	time.Sleep(time.Second)
+	fmt.Println("i: ", i)
+}
+
 func main() {
+	mTest()
+}
+
+func _main() {
 	a := accounts_store.NewAccount(10242048)
 	a.Apply(accounts_store.NewOperation(1, 10242048, +10, "deposit"))
 	a.Apply(accounts_store.NewOperation(2, 10242048, +100, "transfer"))
